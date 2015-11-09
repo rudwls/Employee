187d77bb1f2fa40781f94530bfa1946ea36dcb4b
kjlee <pot030@naver.com>
2015-11-09 16:55:00 +0900

    Member CRUD
    Create	->insert into member ~ values ~
    Read		->select *from memeber
    Update	->update member set ~
    Delete	->delete from member

--- src/test/resources/sql/world.sql
+++ src/test/resources/sql/world.sql
@@ -1,1 +1,21 @@
 select * from country;
+
+drop table member;
+create table member(
+	id 				int auto_increment,
+	email 			varchar(255),
+	name	 		varchar(255),
+	password 		varchar(255),
+	register_date	date,
+	constraint pk_id primary key (id)
+);
+-- Member CRUD (create, read, update, delete )
+--create
+insert into member
+(email, name, password, register_date)
+values
+('xxx@yyy', '홍길동', '1234', '2015/11/11')
+--read
+select * from member
+--deleate
+delete from member;
